e40e048363b112f8fb7aa532d2dcc08549ea8056
mrieramar <mrieramar@uoc.edu>
2024-02-22 10:39:40 +0100

    added elint and fixed bugs

--- Models/Entry.js
+++ Models/Entry.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose'); 
+
 mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 console.log(url);
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {    console.log('connected to MongoDB')  })  
-  .catch((error) => {    console.log('error connecting to MongoDB:', error.message)  })
+  .then((result) => { console.log('connected to MongoDB') })
+  .catch((error) => { console.log('error connecting to MongoDB:', error.message) }))
 
 const entrySchema = new mongoose.Schema({
     name: {
