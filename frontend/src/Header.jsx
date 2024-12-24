8c1e9361263482f12bdfb356d6da438c1db5aada
Monserrate Riera Martí <mrieramar@uoc.edu>
2024-12-24 09:15:34 +0100

    updated title

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -3,7 +3,7 @@ import {Container} from 'react-bootstrap'
 const Head = () => {
     return (
         <Container className='text-bg-primary p-3 text-center'>
-            <h1 className='display-1'>Phonebook</h1>
+            <h1 className='display-1'>Phonebook with docker</h1>
         </Container>
     )
 };
